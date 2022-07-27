ad1c34b98875def2204436cdc737aed1110b791b
Anant Aneja <anant@ahana.io>
2022-07-27 11:00:26 -0700

    Fix SSM access

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@
 variable "base_install" {
   type     = string
   nullable = false
-  default = <<EOF
+  default  = <<EOF
 cd /tmp
 
 sudo apt-get -y update
@@ -11,12 +11,6 @@ sudo yum -y install git htop
 echo "Installing Azul JDK"
 wget -q https://cdn.azul.com/zulu/bin/zulu17.36.13-ca-jdk17.0.4-linux.x86_64.rpm
 sudo yum install -y zulu17.36.13-ca-jdk17.0.4-linux.x86_64.rpm
-
-echo "Installed Systems Manager"
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-sudo systemctl enable amazon-ssm-agent
-sudo systemctl start amazon-ssm-agent
-
 EOF
 }
 
@@ -32,6 +26,33 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
+resource "aws_iam_instance_profile" "dev-resources-iam-profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.dev-resources-iam-role.name
+}
+resource "aws_iam_role" "dev-resources-iam-role" {
+  name               = "dev-ssm-role"
+  description        = "The role for the developer resources EC2"
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": {
+"Effect": "Allow",
+"Principal": {"Service": "ec2.amazonaws.com"},
+"Action": "sts:AssumeRole"
+}
+}
+EOF
+  tags = {
+    stack = "test"
+  }
+}
+resource "aws_iam_role_policy_attachment" "dev-resources-ssm-policy" {
+  role       = aws_iam_role.dev-resources-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
 // Configure the EC2 instance in a public subnet
 resource "aws_instance" "ec2_public" {
   ami                         = data.aws_ami.amazon-linux-2.id
@@ -40,7 +61,8 @@ resource "aws_instance" "ec2_public" {
   key_name                    = var.key_name
   subnet_id                   = var.vpc.public_subnets[0]
   vpc_security_group_ids      = [var.sg_pub_id]
-  
+  iam_instance_profile        = aws_iam_instance_profile.dev-resources-iam-profile.name
+
   user_data = <<EOF
 #!/bin/bash
 echo "127.0.0.1 coordinator" >> /etc/hosts
@@ -51,7 +73,7 @@ ${var.base_install}
 EOF
 
   tags = {
-    "Name" = "COORDINATOR",
+    "Name"     = "COORDINATOR",
     "trino-id" = "COORDINATOR"
   }
 
@@ -65,8 +87,8 @@ resource "aws_instance" "ec2_private" {
   key_name                    = var.key_name
   subnet_id                   = var.vpc.public_subnets[0]
   vpc_security_group_ids      = [var.sg_pub_id]
-
-  user_data = <<EOF
+  iam_instance_profile        = aws_iam_instance_profile.dev-resources-iam-profile.name
+  user_data                   = <<EOF
 #!/bin/bash
 
 echo "Adding Coordinator IP to hosts"
@@ -75,10 +97,10 @@ echo "${aws_instance.ec2_public.private_ip} coordinator" >> /etc/hosts
 ${var.base_install}
 EOF
 
-  count                       = 3
+  count = 4
 
   tags = {
-    "Name" = "WORKER",
+    "Name"     = "WORKER",
     "trino-id" = "WORKER"
   }
 
